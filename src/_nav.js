1d4c44dbbc113a663c67f62649280e58af1df62e
godgodwinter <kakadz.pro@gmail.com>
2021-12-03 12:03:05 +0700

    Initial commit

---
+++ src/_nav.js
@@ -0,0 +1,103 @@
+export default [
+  {
+    component: 'CNavItem',
+    name: 'Dashboard',
+    to: '/dashboard',
+    icon: 'cil-speedometer',
+    badge: {
+      color: 'primary',
+      text: 'NEW',
+    },
+  },
+  {
+    component: 'CNavTitle',
+    name: 'Master',
+  },
+  {
+    component: 'CNavItem',
+    name: 'Sekolah',
+    to: '/sekolah',
+    icon: 'cil-drop',
+  },
+  {
+    component: 'CNavTitle',
+    name: 'SubMenu',
+  },
+  {
+    component: 'CNavItem',
+    name: 'Siswa',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+  {
+    component: 'CNavItem',
+    name: 'Guru atau WaliKelas',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+  {
+    component: 'CNavItem',
+    name: 'Kelas',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+  {
+    component: 'CNavItem',
+    name: 'Nilai',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+  {
+    component: 'CNavItem',
+    name: 'Minat dan Bakat',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+  {
+    component: 'CNavItem',
+    name: 'Penjurusan',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+  {
+    component: 'CNavItem',
+    name: 'Hasil',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+  {
+    component: 'CNavItem',
+    name: 'Kasus Siswa',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+  {
+    component: 'CNavItem',
+    name: 'Pengembangan Diri',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+  {
+    component: 'CNavItem',
+    name: 'Prestasi Siswa',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+
+  // {
+  //   component: 'CNavItem',
+  //   name: 'Download CoreUI',
+  //   href: 'http://coreui.io/vue/',
+  //   icon: { name: 'cil-cloud-download', class: 'text-white' },
+  //   _class: 'bg-success text-white',
+  //   target: '_blank'
+  // },
+  // {
+  //   component: 'CNavItem',
+  //   name: 'Try CoreUI PRO',
+  //   href: 'http://coreui.io/pro/vue/',
+  //   icon: { name: 'cil-layers', class: 'text-white' },
+  //   _class: 'bg-danger text-white',
+  //   target: '_blank'
+  // }
+]
---
+++ src/views/yayasan/Sekolah.vue
@@ -0,0 +1,78 @@
+<template>
+  <CRow>
+    <CCol>
+      <CCard>
+        <CCardHeader> <CIcon icon="cil-drop" /> Data Sekolah </CCardHeader>
+
+
+
+    <CCol :xs="12">
+      <CCard >
+
+
+        <dataSekolah></dataSekolah>
+
+        <dataContoh></dataContoh>
+
+
+      <div class="mt-2 px-2">
+<button class="btn btn-info btn-round">Tambah</button>
+      </div>
+        <CCardBody>
+          <p class="text-medium-emphasis small">
+            Using the most basic table CoreUI, here&#39;s how
+            <code>&lt;CTable&gt;</code>-based tables look in CoreUI.
+          </p>
+          <DocsExample href="components/table.html">
+            <CTable>
+              <CTableHead>
+                <CTableRow>
+                  <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Class</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
+                </CTableRow>
+              </CTableHead>
+              <CTableBody>
+                <CTableRow>
+                  <CTableHeaderCell scope="row">1</CTableHeaderCell>
+                  <CTableDataCell>Mark</CTableDataCell>
+                  <CTableDataCell>Otto</CTableDataCell>
+                  <CTableDataCell>@mdo</CTableDataCell>
+                </CTableRow>
+                <CTableRow>
+                  <CTableHeaderCell scope="row">2</CTableHeaderCell>
+                  <CTableDataCell>Jacob</CTableDataCell>
+                  <CTableDataCell>Thornton</CTableDataCell>
+                  <CTableDataCell>@fat</CTableDataCell>
+                </CTableRow>
+                <CTableRow>
+                  <CTableHeaderCell scope="row">3</CTableHeaderCell>
+                  <CTableDataCell colspan="2">Larry the Bird</CTableDataCell>
+                  <CTableDataCell>@twitter</CTableDataCell>
+                </CTableRow>
+              </CTableBody>
+            </CTable>
+          </DocsExample>
+        </CCardBody>
+      </CCard>
+    </CCol>
+      </CCard>
+    </CCol>
+  </CRow>
+</template>
+
+<script>
+import dataSekolah from '@/components/DataTables/dataSekolah.vue';
+import dataContoh from '@/components/DataTables/dataContoh.vue';
+
+export default {
+  name: 'Sekolah',
+  components: {
+    dataSekolah,
+    dataContoh,
+  },
+
+}
+</script>
+
